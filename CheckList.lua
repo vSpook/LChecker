439db7caa329fa006a0f6c36e040cbaaeb1753b6
GSK <130244434+vSpook@users.noreply.github.com>
2023-04-08 13:54:55 -0400

    Add files via upload

---
+++ CheckList.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SrGask/LChecker/main/TheCode", true))()
---
+++ WhiteList.lua
@@ -0,0 +1,12 @@
+print("[TC]: WhiteList Loaded")
+
+Admiteds = {
+	[456003260] = true, -- The Masive Monke
+	[3306969602] = true, -- Adrian
+	[211345193] = true, -- tavo72
+	[2771502217] = true, -- Sofi <3
+	[3287500413] = true,
+	[1] = true,
+}
+
+return Admiteds
